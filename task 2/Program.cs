cb5699066d770b537271b3e3ffd1ab7e41488536
vladimir <euthavova@mail.ru>
2024-10-31 21:09:48 +1000

    added task 2

---
+++ task 2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+void FindSumInCols(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < cols; j++)
+        {
+            sum += array[i, j];
+        }
+        Console.Write($"Sum of elements in column {cols}: {sum}");
+        Console.WriteLine();
+    }
+}
+
+int[,] array =
+{
+    {9,8,7},
+    {7,8,9},
+    {4,5,6},
+};
+FindSumInCols(array);
