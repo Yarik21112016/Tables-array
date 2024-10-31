3343b201d98c7f2c554deff4e22b8d9771cef0bd
vladimir <euthavova@mail.ru>
2024-10-31 21:10:03 +1000

    added task 3

---
+++ task 3/Program.cs
@@ -0,0 +1,35 @@
+﻿
+void ColsToRows(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    int[,] trans = new int[cols,rows];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            trans[j,i] = array[i,j];
+        }
+    }
+    Console.Write("Transpose matrix:");
+    Console.WriteLine(" ");
+    for (int i = 0; i < cols; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            Console.Write(trans[i,j] + " ");
+            Console.Write(" ");
+        }
+        Console.Write(" ");
+    }
+}
+
+int[,] array =
+{
+    {1,2,3},
+    {4,5,6},
+    {7,8,9},
+};
+
+ColsToRows(array);
